a2971995d475481cbc441bf404eb55e3e7fbc036
bramdenelzen <bram.denelzen@gmail.com>
2026-01-20 17:10:57 +0100

    added my name to the page and a github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
-import { Typography } from "@/components/ui/typography"
-import { ModeToggle } from "@/components/mode-toggle"
-import { InstallationSection } from "@/components/sections/installation-section"
-import { ExampleCodeSection } from "@/components/sections/example-code-section"
-import { DefaultVariantsSection } from "@/components/sections/default-variants-section"
+import { Typography } from "@/components/ui/typography";
+import { ModeToggle } from "@/components/mode-toggle";
+import { InstallationSection } from "@/components/sections/installation-section";
+import { ExampleCodeSection } from "@/components/sections/example-code-section";
+import { DefaultVariantsSection } from "@/components/sections/default-variants-section";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Github, ExternalLink } from "lucide-react";
 
 export default async function Home() {
   return (
     <div className="container max-w-3xl mx-auto py-10 px-4 space-y-10 relative">
-      <div className="absolute right-4 top-14">
+      <div className="absolute right-4 top-14 flex items-center gap-2">
+        <Button variant="outline" size="icon" asChild>
+          <Link
+            href="https://github.com/bramdenelzen/shadcn-typography"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github className="h-[1.2rem] w-[1.2rem]" />
+          </Link>
+        </Button>
         <ModeToggle />
       </div>
       <div className="space-y-24">
@@ -26,8 +38,21 @@ export default async function Home() {
         <ExampleCodeSection />
 
         <DefaultVariantsSection />
+
+        <div className="flex justify-center pb-8">
+          <Typography variant={"small"}>
+            <span className="text-muted-foreground">Made by </span>
+            <Link
+              className="italic group inline-flex items-center gap-1"
+              href="https://github.com/bramdenelzen"
+              target="_blank"
+            >
+              Bram den Elzen
+              <ExternalLink className="h-3 w-3 opacity-0 transition-opacity group-hover:opacity-100" />
+            </Link>
+          </Typography>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-
